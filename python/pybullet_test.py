516b5b620d6601a5c9e7cf814b0098a8dfbbfa54
Chuanhua Liu <liuchuany6@gmail.com>
2025-03-31 15:06:53 +0800

    implemented 3 fingered urdf, IK familiarisation and testing

--- python/pybullet_test.py
+++ python/pybullet_test.py
@@ -92,13 +92,16 @@ class LeapPybulletIK():
         self.glove_to_leap_mapping_scale = 1
         path_src = os.path.abspath(__file__)
         path_src = os.path.dirname(path_src)
-        path_src = os.path.join(path_src, "leap_hand_mesh_right/robot_pybullet.urdf")
+        path_src = os.path.join(path_src, "for_urdf_export/for_urdf_export.urdf")
+        self.leapEndEffectorIndex = [2, 3, 6, 7]
         self.LeapId = p.loadURDF(
             path_src,
             # [-0.05, -0.03, -0.125],
-            [0,0,0],
-            p.getQuaternionFromEuler([0, 0, 1.57]),
-            useFixedBase = True
+            [0.05,0.079,0],
+            p.getQuaternionFromEuler([0, 0, 3.14]),
+            useFixedBase = True,
+            flags=p.URDF_MAINTAIN_LINK_ORDER
+            
         )
         self.numJoints = p.getNumJoints(self.LeapId)
         p.setGravity(0, 0, 0)
@@ -111,37 +114,68 @@ class LeapPybulletIK():
         # p.disconnect()
         
 
+        self.create_initial_vis()
         self.create_target_vis()
 
+    def create_initial_vis(self):
+        small_ball_radius = 0.01
+        small_ball_shape = p.createCollisionShape(p.GEOM_SPHERE, radius=small_ball_radius)
+        ball_radius = .005
+        ball_shape = p.createCollisionShape(p.GEOM_SPHERE, radius=ball_radius)
+        baseMass = 0.001
+        basePosition = [0.25, 0.25, 0]
+        
+        self.ballMbt = []
+        for i in range(0,1):
+            self.ballMbt.append(p.createMultiBody(baseMass=baseMass, baseCollisionShapeIndex=ball_shape, basePosition=basePosition)) # for base and finger tip joints    
+            no_collision_group = 0
+            no_collision_mask = 0
+            p.setCollisionFilterGroupMask(self.ballMbt[i], -1, no_collision_group, no_collision_mask)
+        p.changeVisualShape(self.ballMbt[0], -1, rgbaColor=[1, 0, 0, 1]) 
+        # p.changeVisualShape(self.ballMbt[1], -1, rgbaColor=[0, 1, 0, 1]) 
+        # p.changeVisualShape(self.ballMbt[2], -1, rgbaColor=[0, 0, 1, 1])  
+        # p.changeVisualShape(self.ballMbt[3], -1, rgbaColor=[1, 1, 1, 1])
+
+
     def create_target_vis(self):
-            # load balls
-            small_ball_radius = 0.01
-            small_ball_shape = p.createCollisionShape(p.GEOM_SPHERE, radius=small_ball_radius)
-            ball_radius = 0.01
-            ball_shape = p.createCollisionShape(p.GEOM_SPHERE, radius=ball_radius)
-            baseMass = 0.001
-            basePosition = [0.25, 0.25, 0]
-            
-            self.ballMbt = []
-            for i in range(0,4):
-                self.ballMbt.append(p.createMultiBody(baseMass=baseMass, baseCollisionShapeIndex=ball_shape, basePosition=basePosition)) # for base and finger tip joints    
-                no_collision_group = 0
-                no_collision_mask = 0
-                p.setCollisionFilterGroupMask(self.ballMbt[i], -1, no_collision_group, no_collision_mask)
-            p.changeVisualShape(self.ballMbt[0], -1, rgbaColor=[1, 0, 0, 1]) 
-            p.changeVisualShape(self.ballMbt[1], -1, rgbaColor=[0, 1, 0, 1]) 
-            p.changeVisualShape(self.ballMbt[2], -1, rgbaColor=[0, 0, 1, 1])  
-            p.changeVisualShape(self.ballMbt[3], -1, rgbaColor=[1, 1, 1, 1])
+        # load balls
+        small_ball_radius = 0.01
+        small_ball_shape = p.createCollisionShape(p.GEOM_SPHERE, radius=small_ball_radius)
+        ball_radius = 0.01
+        ball_shape = p.createCollisionShape(p.GEOM_SPHERE, radius=ball_radius)
+        baseMass = 0.001
+        basePosition = [0.25, 0.25, 0]
+
+        self.ballMbt = []
+        for i in range(0,2):
+            self.ballMbt.append(p.createMultiBody(baseMass=baseMass, baseCollisionShapeIndex=ball_shape, basePosition=basePosition)) # for base and finger tip joints    
+            no_collision_group = 0
+            no_collision_mask = 0
+            p.setCollisionFilterGroupMask(self.ballMbt[i], -1, no_collision_group, no_collision_mask)
+        p.changeVisualShape(self.ballMbt[0], -1, rgbaColor=[1, 0, 0, 1]) 
+        p.changeVisualShape(self.ballMbt[1], -1, rgbaColor=[0, 1, 0, 1]) 
+        # p.changeVisualShape(self.ballMbt[2], -1, rgbaColor=[0, 0, 1, 1])  
+        # p.changeVisualShape(self.ballMbt[3], -1, rgbaColor=[1, 1, 1, 1])
     
     def update_target_vis(self, hand_pos):
         _, current_orientation = p.getBasePositionAndOrientation( self.ballMbt[0])
         p.resetBasePositionAndOrientation(self.ballMbt[0], hand_pos[1], current_orientation)
         _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[1])
         p.resetBasePositionAndOrientation(self.ballMbt[1], hand_pos[3], current_orientation)
-        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[2])
-        p.resetBasePositionAndOrientation(self.ballMbt[2], hand_pos[5], current_orientation)
-        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[3])
-        p.resetBasePositionAndOrientation(self.ballMbt[3], hand_pos[7], current_orientation)
+        # _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[2])
+        # p.resetBasePositionAndOrientation(self.ballMbt[2], hand_pos[5], current_orientation)
+        # _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[3])
+        # p.resetBasePositionAndOrientation(self.ballMbt[3], hand_pos[7], current_orientation)
+
+    def update_initial_vis(self, hand_pos):
+        _, current_orientation = p.getBasePositionAndOrientation( self.ballMbt[0])
+        p.resetBasePositionAndOrientation(self.ballMbt[0], hand_pos, current_orientation)
+        # _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[1])
+        # p.resetBasePositionAndOrientation(self.ballMbt[1], hand_pos[1], current_orientation)
+        # _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[2])
+        # p.resetBasePositionAndOrientation(self.ballMbt[2], hand_pos[5], current_orientation)
+        # _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[3])
+        # p.resetBasePositionAndOrientation(self.ballMbt[3], hand_pos[7], current_orientation)
 
     def get_glove_data(self):
         #gets the data converts it and then computes IK and visualizes
@@ -155,8 +189,84 @@ class LeapPybulletIK():
         #hand_pos[2][1] = hand_pos[2][1] + 0.002
         hand_pos[4][1] = hand_pos[4][1] + 0.002
         hand_pos[6][1] = hand_pos[6][1] + 0.002
-        self.compute_IK(hand_pos)
+        # self.compute_IK(hand_pos)
         self.update_target_vis(hand_pos)
+
+    def compute_IK(self, hand_pos):
+        p.stepSimulation()     
+
+        # rightHandIndex_middle_pos = hand_pos[2]
+        # rightHandIndex_pos = hand_pos[3]
+        
+        # rightHandMiddle_middle_pos = hand_pos[4]
+        # rightHandMiddle_pos = hand_pos[5]
+        
+        # rightHandRing_middle_pos = hand_pos[6]
+        # rightHandRing_pos = hand_pos[7]
+        
+        # rightHandThumb_middle_pos = hand_pos[0]
+        # rightHandThumb_pos = hand_pos[1]
+        
+        
+        
+        rightHandMiddle_middle_pos = hand_pos[0]
+        rightHandMiddle_pos = hand_pos[1]
+        
+        rightHandIndex_middle_pos = hand_pos[2]
+        rightHandIndex_pos = hand_pos[3]
+        
+        # rightHandThumb_middle_pos = hand_pos[4]
+        # rightHandThumb_pos = hand_pos[5]
+
+        leapEndEffectorPos = [
+            rightHandMiddle_middle_pos,
+            rightHandMiddle_pos,
+            rightHandIndex_middle_pos,
+            rightHandIndex_pos,
+            # rightHandThumb_middle_pos,
+            # rightHandThumb_pos
+        ]
+
+        jointPoses = p.calculateInverseKinematics2(
+            self.LeapId,
+            self.leapEndEffectorIndex,
+            leapEndEffectorPos,
+            solver=p.IK_DLS,
+            maxNumIterations=50,
+            residualThreshold=0.0001,
+        )
+        # print(jointPoses)
+        
+        # combined_jointPoses = (jointPoses[0:4] + (0.0,) + jointPoses[4:8] + (0.0,) + jointPoses[8:12] + (0.0,) + jointPoses[12:16] + (0.0,))
+        # combined_jointPoses = list(combined_jointPoses)
+
+        # update the hand joints
+        for i in range(12):
+            p.setJointMotorControl2(
+                bodyIndex=self.LeapId,
+                jointIndex=i,
+                controlMode=p.POSITION_CONTROL,
+                targetPosition=jointPoses[i],
+                targetVelocity=0,
+                force=500,
+                positionGain=0.3,
+                velocityGain=1,
+            )
+
+        # map results to real robot
+        # real_robot_hand_q = np.array([float(0.0) for _ in range(16)])
+        # #real_left_robot_hand_q = np.array([0.0 for _ in range(16)])
+
+        # real_robot_hand_q[0:4] = jointPoses[0:4]
+        # real_robot_hand_q[4:8] = jointPoses[4:8]
+        # real_robot_hand_q[8:12] = jointPoses[8:12]
+        # real_robot_hand_q[12:16] = jointPoses[12:16]
+        # real_robot_hand_q[0:2] = real_robot_hand_q[0:2][::-1]
+        # real_robot_hand_q[4:6] = real_robot_hand_q[4:6][::-1]
+        # real_robot_hand_q[8:10] = real_robot_hand_q[8:10][::-1]
+        # stater = JointState()
+        # stater.position = [float(i) for i in real_robot_hand_q]
+        # self.pub_hand.publish(stater)
 
 
 
@@ -176,32 +286,55 @@ def main(**kwargs):
     leappybulletik = LeapPybulletIK()
     glove_to_leap_mapping_scale = 1.6
 
-    while True:
-        message = socket.recv()
-        #receive the message from the socket
-        message = message.decode('utf-8')
+    # n = p.getLinkState(leappybulletik.LeapId, 3)
+    # print(n)
 
-        # right[0] is gloveID, the nodes used for IK are (3, 4, 8, 9, 13, 14, 18, 19, 23, 24)
-        # right[1:31] are the x,y,z values for the above nodes; 10 nodes * 3 axes = 30 values
-        right = message.split(",")   
-        right = list(map(float,right[1:31]))
-        right = [round(n, 2) for n in right]
+    hand_pos = []
+    linkStates = p.getLinkStates(leappybulletik.LeapId, leappybulletik.leapEndEffectorIndex)
+    for e in linkStates:
+        hand_pos.append(e[4])
+    print("Initial pos:")
+    print(hand_pos)
 
-        hand_pos = []  
-        for i in range(0,10):
-            hand_pos.append([right[i * 3] * glove_to_leap_mapping_scale * 1.15, right[i * 3 + 1]* glove_to_leap_mapping_scale, -i * right[i * 3 + 2] * glove_to_leap_mapping_scale])
-        # hand_pos[2][0] = hand_pos[2][0] - 0.02  this isn't great because they won't oppose properly
-        # hand_pos[3][0] = hand_pos[3][0] - 0.02    
-        # hand_pos[6][0] = hand_pos[6][0] + 0.02
-        # hand_pos[7][0] = hand_pos[7][0] + 0.02
-        #hand_pos[2][1] = hand_pos[2][1] + 0.002
-        hand_pos[4][1] = hand_pos[4][1] + 0.002
-        hand_pos[6][1] = hand_pos[6][1] + 0.002
-        # compute_IK(hand_pos)
+    for n in range(100):        
+        buf_pos = []
+        # p.stepSimulation()
+        for count in range(4):
+            buf_pos.append((hand_pos[count][0], hand_pos[count][1] - 0.0008, hand_pos[count][2] - 0.0005))
+        hand_pos = buf_pos
+
+        print("New pos" , n)
+        print(hand_pos)
 
         leappybulletik.update_target_vis(hand_pos)
-        print(hand_pos)
-        time.sleep(.1)
+        leappybulletik.compute_IK(hand_pos)
+        # message = socket.recv()
+        # ##receive the message from the socket
+        # message = message.decode('utf-8')
+
+        # ## right[0] is gloveID, the nodes used for IK are (3, 4, 8, 9, 13, 14, 18, 19, 23, 24)
+        # ## right[1:31] are the x,y,z values for the above nodes; 10 nodes * 3 axes = 30 values
+        # right = message.split(",")   
+        # right = list(map(float,right[1:31]))
+        # right = [round(n, 2) for n in right]
+
+        # hand_pos = []  
+        # for i in range(0,10):
+        #     hand_pos.append([right[i * 3] * glove_to_leap_mapping_scale * 1.15, right[i * 3 + 1]* glove_to_leap_mapping_scale, -i * right[i * 3 + 2] * glove_to_leap_mapping_scale])
+        # # hand_pos[2][0] = hand_pos[2][0] - 0.02  this isn't great because they won't oppose properly
+        # # hand_pos[3][0] = hand_pos[3][0] - 0.02    
+        # # hand_pos[6][0] = hand_pos[6][0] + 0.02
+        # # hand_pos[7][0] = hand_pos[7][0] + 0.02
+        # #hand_pos[2][1] = hand_pos[2][1] + 0.002
+        # hand_pos[4][1] = hand_pos[4][1] + 0.002
+        # hand_pos[6][1] = hand_pos[6][1] + 0.002
+        # # compute_IK(hand_pos)
+
+        # leappybulletik.update_target_vis(hand_pos)
+    # print(hand_pos)
+        time.sleep(.5)
+
+    # p.disconnect()
 
 if __name__ == "__main__":
     main()
