27a92772c3d62ff1cacc53c4eed4ea0fb48a3e6e
Chicha6 <liuchuany6@gmail.com>
2025-04-11 16:36:26 +0800

    recommit

--- python/manusleap_ik.py
+++ python/manusleap_ik.py
@@ -1,13 +1,12 @@
 import pybullet as p
 import time
-import pybullet_data
 import os
 import numpy as np
 import zmq
 from leap_hand_utils.dynamixel_client import *
 import leap_hand_utils.leap_hand_utils as lhu
 import time
-from scipy.spatial.transform import Rotation as R
+import sys
 
 
 class LeapNode:
@@ -26,7 +25,8 @@ class LeapNode:
         # For example ls /dev/serial/by-id/* to find your LEAP Hand. Then use the result.  
         # For example: /dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT7W91VW-if00-port0
 
-        self.motors = motors = [0,1,2,3,4,5,6,7,8,9,10,11]
+        self.motors = motors = [1,0,2,3,5,4,6,7,9,8,10,11]
+        # self.motors = motors = [1,0,2,3,5,4,6,7]
 
 
         try:
@@ -85,46 +85,39 @@ class LeapNode:
         return self.dxl_client.read_pos_vel_cur()
 
 class LeapPybulletIK():
-    def __init__(self):
-        # super().__init__('leap_pyb_ik')  
-        # start pybullet
-        #clid = p.connect(p.SHARED_MEMORY)
-        #clid = p.connect(p.DIRECT)
+    def __init__(self, basePosAndScaling=None):
+        
+        #start pybullet
         physicsClient = p.connect(p.GUI)
+        self.basePosAndScaling = basePosAndScaling
         self.glove_to_leap_mapping_scale = 1
         path_src = os.path.abspath(__file__)
         path_src = os.path.dirname(path_src)
-        path_src = os.path.join(path_src, "for_urdf_export/for_urdf_export.urdf")
-        # here endEffector refers to the target joints(not link)
-        # by right its [3,4,8,9,13,14]
-        self.leapEndEffectorIndex = [3, 4, 8, 9,13,14]
-        self.LeapId = p.loadURDF(
+        path_src = os.path.join(path_src, "right_hand_mesh/right_hand.urdf")
+
+        # Here endEffector refers to the target joints(not links)
+        # These correspond to the dip joints and fingertips of thumb, index, and middle
+        self.leapEndEffectorIndex = [3, 4, 8, 9, 13, 14]
+        self.LeapId = p.loadURDF( 
             path_src,
-            # [-0.05, -0.03, -0.125],
-            [0.015,0.1,-0.008],
+            [basePosAndScaling[0], basePosAndScaling[1], basePosAndScaling[2]] if self.basePosAndScaling != None 
+            else [0.019,0.120,-0.016],
             p.getQuaternionFromEuler([0, 0, 3.14]),
             useFixedBase = True,
             flags=p.URDF_MAINTAIN_LINK_ORDER,
-            globalScaling = 0.55
-            
         )
         self.numJoints = p.getNumJoints(self.LeapId)
         p.setGravity(0, 0, 0)
         useRealTimeSimulation = 0
         p.setRealTimeSimulation(useRealTimeSimulation)
 
-        # self.create_initial_vis()
         self.create_target_vis()
 
     def create_target_vis(self):
-        # load balls
-        small_ball_radius = 0.01
-        small_ball_shape = p.createCollisionShape(p.GEOM_SPHERE, radius=small_ball_radius)
         ball_radius = 0.003
         ball_shape = p.createCollisionShape(p.GEOM_SPHERE, radius=ball_radius)
         baseMass = 0.001
         basePosition = [0.25, 0.25, 0]
-
         self.ballMbt = []
         for i in range(0,6):
             self.ballMbt.append(p.createMultiBody(baseMass=baseMass, baseCollisionShapeIndex=ball_shape, basePosition=basePosition)) # for base and finger tip joints    
@@ -138,35 +131,37 @@ class LeapPybulletIK():
         p.changeVisualShape(self.ballMbt[4], -1, rgbaColor=[0, 0, 1, 1])  
         p.changeVisualShape(self.ballMbt[5], -1, rgbaColor=[1, 0, 1, 1])
     
-    def update_target_vis(self, hand_pos):
-        _, current_orientation = p.getBasePositionAndOrientation( self.ballMbt[0])
-        p.resetBasePositionAndOrientation(self.ballMbt[0], hand_pos[0], current_orientation)
-        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[1])
-        p.resetBasePositionAndOrientation(self.ballMbt[1], hand_pos[1], current_orientation)
-        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[2])
-        p.resetBasePositionAndOrientation(self.ballMbt[2], hand_pos[2], current_orientation)
-        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[3])
-        p.resetBasePositionAndOrientation(self.ballMbt[3], hand_pos[3], current_orientation)
-        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[4])
-        p.resetBasePositionAndOrientation(self.ballMbt[4], hand_pos[4], current_orientation)
-        _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[5])
-        p.resetBasePositionAndOrientation(self.ballMbt[5], hand_pos[5], current_orientation)
+    def update_target_vis(self, hand_pos, enable_offset):
+        if (enable_offset):
+            _, current_orientation = p.getBasePositionAndOrientation( self.ballMbt[0])
+            p.resetBasePositionAndOrientation(self.ballMbt[0], hand_pos[0], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[1])
+            p.resetBasePositionAndOrientation(self.ballMbt[1], hand_pos[1], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[2])
+            p.resetBasePositionAndOrientation(self.ballMbt[2], [hand_pos[2][0] + 0.02, hand_pos[2][1], hand_pos[2][2]], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[3])
+            p.resetBasePositionAndOrientation(self.ballMbt[3], [hand_pos[3][0] + 0.02, hand_pos[3][1], hand_pos[3][2]], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[4])
+            p.resetBasePositionAndOrientation(self.ballMbt[4], [hand_pos[4][0] + 0.02, hand_pos[4][1], hand_pos[4][2]], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[5])
+            p.resetBasePositionAndOrientation(self.ballMbt[5], [hand_pos[5][0] + 0.02, hand_pos[5][1], hand_pos[5][2]] , current_orientation)
+        else:
+            _, current_orientation = p.getBasePositionAndOrientation( self.ballMbt[0])
+            p.resetBasePositionAndOrientation(self.ballMbt[0], hand_pos[0], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[1])
+            p.resetBasePositionAndOrientation(self.ballMbt[1], hand_pos[1], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[2])
+            p.resetBasePositionAndOrientation(self.ballMbt[2], hand_pos[2], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[3])
+            p.resetBasePositionAndOrientation(self.ballMbt[3], hand_pos[3], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[4])
+            p.resetBasePositionAndOrientation(self.ballMbt[4], hand_pos[4], current_orientation)
+            _, current_orientation = p.getBasePositionAndOrientation(self.ballMbt[5])
+            p.resetBasePositionAndOrientation(self.ballMbt[5], hand_pos[5], current_orientation)     
 
-    def get_glove_data(self):
-        #gets the data converts it and then computes IK and visualizes
-        hand_pos = []  
-        # for i in range(0,10):
-        #     hand_pos.append([poses[i].position.x * self.glove_to_leap_mapping_scale * 1.15, poses[i].position.y * self.glove_to_leap_mapping_scale, -poses[i].position.z * self.glove_to_leap_mapping_scale])
-        # hand_pos[2][0] = hand_pos[2][0] - 0.02  this isn't great because they won't oppose properly
-        # hand_pos[3][0] = hand_pos[3][0] - 0.02    
-        # hand_pos[6][0] = hand_pos[6][0] + 0.02
-        # hand_pos[7][0] = hand_pos[7][0] + 0.02
-        #hand_pos[2][1] = hand_pos[2][1] + 0.002
-        hand_pos[4][1] = hand_pos[4][1] + 0.002
-        hand_pos[6][1] = hand_pos[6][1] + 0.002
-        # self.compute_IK(hand_pos)
-        self.update_target_vis(hand_pos)
+        
 
+    # Computes and returns joint angles using IK and updates joints in simulation
     def compute_IK(self, hand_pos):
         p.stepSimulation()     
 
@@ -179,10 +174,7 @@ class LeapPybulletIK():
         rightHandMiddle_middle_pos = hand_pos[4]
         rightHandMiddle_pos = hand_pos[5]
         
-        
-
         leapEndEffectorPos = [
-
             rightHandIndex_middle_pos,
             rightHandIndex_pos,
             rightHandMiddle_middle_pos,
@@ -191,7 +183,7 @@ class LeapPybulletIK():
             rightHandThumb_pos,
         ]
 
-        # returns resultant joint angles
+        # Returns resultant joint angles
         jointPoses = p.calculateInverseKinematics2(
             self.LeapId,
             self.leapEndEffectorIndex,
@@ -200,12 +192,15 @@ class LeapPybulletIK():
             maxNumIterations=50,
             residualThreshold=0.0001,
         )
-        # print(len(jointPoses))
         
+        # Additional zero values are for the rigid joints between fingertip and realtip
+        # Because the above IK function takes the origin of the joint of the child link as the coordinates of the child link.
+        # Hence to reference the extreme tip of the finger for IK, a realtip link was created at the end of each finger, with the joint positioned at the tip.
+        # The additional realtip joint at each of the 3 fingers results in a total of 15 joints (rather than 12).
+        # These additional joints are rigid, but stil recognised by setJointMotorControl2(), hence the 3 additional zero values
         combined_jointPoses = (jointPoses[0:4] + (0.0,) + jointPoses[4:8] + (0.0,) + jointPoses[8:12] + (0.0,))
-        # print(combined_jointPoses[0:5])
 
-        # update the hand joints in simulator
+        # Updates the hand joints in simulator
         for i in range(15):
             p.setJointMotorControl2(
                 bodyIndex=self.LeapId,
@@ -217,60 +212,99 @@ class LeapPybulletIK():
                 positionGain=0.3,
                 velocityGain=1,
             )
+        return jointPoses
 
-        # map results to real robot
-        # real_robot_hand_q = np.array([float(0.0) for _ in range(16)])
-        # #real_left_robot_hand_q = np.array([0.0 for _ in range(16)])
+def pybulletCalibration(socket):
+    avgSampleIndexMiddle = [0,0,0,0,0,0,0,0,0,0,0,0]
+    avgSampleThumb = [0,0,0,0,0,0]
+    calibrationPoints = []
+    URDFBaseOffset = [0,0,0]
+    ManusToLeapScaling = [0,0,0]
 
-        # real_robot_hand_q[0:4] = jointPoses[0:4]
-        # real_robot_hand_q[4:8] = jointPoses[4:8]
-        # real_robot_hand_q[8:12] = jointPoses[8:12]
-        # real_robot_hand_q[12:16] = jointPoses[12:16]
-        # real_robot_hand_q[0:2] = real_robot_hand_q[0:2][::-1]
-        # real_robot_hand_q[4:6] = real_robot_hand_q[4:6][::-1]
-        # real_robot_hand_q[8:10] = real_robot_hand_q[8:10][::-1]
-        # stater = JointState()
-        # stater.position = [float(i) for i in real_robot_hand_q]
-        # self.pub_hand.publish(stater)
+    input("Press Enter to start index and middle sampling. Make sure you're in the correct calibration pose!")
+    # Index and middle fingers sampling
+    # Calibration pose: all fingers relaxed and closed, flat on table
+    for n in range(50):
+        message = socket.recv()
+        message = message.decode('utf-8')
+        manusData = message.split(",")
+        currentSample = list(map(float,manusData[7:]))
 
+        for m in range(len(currentSample)):
+            if (avgSampleIndexMiddle[m] != 0):
+                avgSampleIndexMiddle[m] = (avgSampleIndexMiddle[m] + currentSample[m]) / 2
+            else:
+                avgSampleIndexMiddle[m] = currentSample[m]
+    
+    input("Press Enter to start thumb sampling. Make sure you're in the correct calibration pose!")
+    # Thumb sampling
+    # Calibration pose: all fingers relaxed and closed, flat on table, thumb pointing outwards a far as possible while keeping it straight
+    for n in range(50):
+        message = socket.recv()
+        message = message.decode('utf-8')
+        manusData = message.split(",")
+        currentSample = list(map(float,manusData[1:7]))
+
+        for m in range(6):
+            if (avgSampleThumb[m] != 0):
+                avgSampleThumb[m] = (avgSampleThumb[m] + currentSample[m]) / 2
+            else:
+                avgSampleThumb[m] = currentSample[m]
+
+    calibrationPoints.append(avgSampleThumb[0:3]) #thumb_distal
+    calibrationPoints.append(avgSampleThumb[3:6]) #thumb_tip
+    calibrationPoints.append(avgSampleIndexMiddle[0:3]) #index_distal
+    calibrationPoints.append(avgSampleIndexMiddle[3:6]) #index_tip
+    calibrationPoints.append(avgSampleIndexMiddle[6:9]) #middle_distal
+    calibrationPoints.append(avgSampleIndexMiddle[9:12]) #middle_tip
+
+    URDFBaseOffset[0] = 0.02 + calibrationPoints[5][0] #URDF base x-position
+    URDFBaseOffset[1] = 0.062 + (calibrationPoints[0][1] + calibrationPoints[1][1]) / 2 #URDF base y-position
+    URDFBaseOffset[2] = -0.007 #URDF base z-position
+    ManusToLeapScaling[0] = (-0.209 + URDFBaseOffset[0]) / calibrationPoints[1][0] #thumb scaling
+    ManusToLeapScaling[1] = (0.152 + URDFBaseOffset[1]) / calibrationPoints[3][1] #index scaling 
+    ManusToLeapScaling[2] = (0.152 + URDFBaseOffset[1]) / calibrationPoints[5][1] #middle scaling
+    
+    print(URDFBaseOffset)
+    return URDFBaseOffset, ManusToLeapScaling
+    
 
 
 def main(**kwargs):
-    # leap_hand = LeapNode()
 
+    # leap_hand = LeapNode()
     context = zmq.Context()
     #Socket to talk to Manus SDK
     print("Connecting to SDK")
     socket = context.socket(zmq.PULL)
     socket.setsockopt(zmq.CONFLATE, True)     
     socket.connect("tcp://127.0.0.1:8000")
-
-    leappybulletik = LeapPybulletIK()
-
-    # iniPos = []
+    URDFbaseOffset = []
+    ManusToLeapScaling = []
     targetPosList = []
-    transformPosList = []
+    leapInputAngles = []
+    isCalibrated = False
 
-    # linkStates = p.getLinkStates(leappybulletik.LeapId, leappybulletik.leapEndEffectorIndex)
+    # Generate calibration data (optional) and initialise pybullet
+    # Usage: "python MANUS_IK.py c" or "python MANUS_IK.py C"
+    if(sys.argv[1] == "c" or sys.argv[1] == "C"):
+        URDFbaseOffset, ManusToLeapScaling = pybulletCalibration(socket)
+        print(URDFbaseOffset, ManusToLeapScaling)
+        leappybulletik = LeapPybulletIK(URDFbaseOffset)
+        isCalibrated = True
+    else:
+        leappybulletik = LeapPybulletIK()
 
     for n in range(10000):        
         targetPosList.clear()
-        transformPosList.clear()
-        
-        # for i in range (4):
-        #     newPos.append((iniPos[i][0], iniPos[i][1] - 0.001 * n, iniPos[i][2] - 0.002 * n))
-        # print(newPos)
-        
-
+        leapInputAngles.clear()
+    
         message = socket.recv()
         message = message.decode('utf-8')
         manusData = message.split(",")   
 
         # gloveID = manusData[0]
-        axisRot = list(map(float,manusData[1:5]))
-        targetCoord = list(map(float,manusData[5:]))
-        
-
+        targetCoord = list(map(float,manusData[1:]))
         targetPosList.append(targetCoord[0:3]) #thumb_distal
         targetPosList.append(targetCoord[3:6]) #thumb_tip
         targetPosList.append(targetCoord[6:9]) #index_distal
@@ -278,18 +312,45 @@ def main(**kwargs):
         targetPosList.append(targetCoord[12:15]) #middle_distal
         targetPosList.append(targetCoord[15:18]) #middle_tip
 
-        #applying vector transformation to generate unrotated coordinates of target pose
-        r = R.from_quat([axisRot[0], axisRot[1], axisRot[2], axisRot[3]])
-        
-        for count in range(6):
-            transformVec = r.apply(targetPosList[count], inverse = True)
-            transformPosList.append(transformVec)
+        # MANUS to LEAP scaling
+        if (isCalibrated):
+            for n in range(len(targetPosList)):
+                for i in range(3):
+                    if(n == 0 or n == 1):
+                        targetPosList[n][i] = targetPosList[n][i] * ManusToLeapScaling[0]
+                    elif(n == 2 or n == 3):
+                        if(i == 0):
+                            targetPosList[n][i] -= .015
+                        targetPosList[n][i] = targetPosList[n][i] * ManusToLeapScaling[1]
+                    else:
+                        targetPosList[n][i] = targetPosList[n][i] * ManusToLeapScaling[2]
+        else:        
+            # Manually change MANUS to LEAP scaling if user chose not to calibrate 
+            for n in range(len(targetPosList)):
+                for i in range(3):
+                    if(n == 0 or n == 1):
+                        targetPosList[n][i] = targetPosList[n][i] * 1.45
+                    elif(n == 2 or n == 3):
+                        if(i == 0):
+                            targetPosList[n][i] -= .015
+                        targetPosList[n][i] = targetPosList[n][i] * 1.42
+                    else:
+                        targetPosList[n][i] = targetPosList[n][i] * 1.35
 
-        leappybulletik.update_target_vis(transformPosList)
-        leappybulletik.compute_IK(transformPosList)
+        leappybulletik.update_target_vis(targetPosList, False)
+        IKAngles = leappybulletik.compute_IK(targetPosList)
 
+        # joint angle polarity and magnitude correction for leap control
+        for count in range(len(IKAngles)):
+            if(count == 1 or count == 5):
+                leapInputAngles.append(IKAngles[count] * -1 + 3.14)
+            elif(count == 8 or count == 9 or count == 10 or count == 11 ):
+                leapInputAngles.append(IKAngles[count] * -1 + 3.14)
+            else:
+                leapInputAngles.append(IKAngles[count] + 3.14)
 
-        time.sleep(.01)
+        # leap_hand.set_leap(leapInputAngles)
+        time.sleep(.02)
 
     p.disconnect()
 
